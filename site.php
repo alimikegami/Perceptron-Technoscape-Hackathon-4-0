f24ab5c555886d06e155e74d44a75b91866d8fac
Arlan Kusuma Ardika <landarlan60@gmail.com>
2021-07-04 00:56:22 +0800

    Adding site.php and developing it

---
+++ site.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lihat Penginapan: <?php?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/7c6955c5b0.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- Header -->
+    <?php include 'header.php' ?>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="hsla(204,60%,50%,1)" fill-opacity="1" d="M0,160L48,138.7C96,117,192,75,288,53.3C384,32,480,32,576,64C672,96,768,160,864,170.7C960,181,1056,139,1152,112C1248,85,1344,75,1392,69.3L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+
+    <!-- Content -->
+    <br><br>
+    <section>
+        <div class="container text-center" id="site-details">
+            <div class="row row-cols-2 row-cols-md-2 g-2 g-md-3">
+                <div class="col text-end">
+                    <img src="img/dummy-site.png" alt="site" style="width: 350px;" class="align-top">
+                </div>
+                <div class="col text-start">
+                    <h4 class="font-weight-bold"><strong>
+                        Penginapan 5
+                    </strong></h4>
+                    <br>
+                    <!-- Iteration starts -->
+                    <h5 style="color: rgb(255, 215, 0);"><i class="fas fa-star"></i> <i class="fas fa-star"></i></h5>
+                    <!-- Iteration ends -->
+                    <br>
+                    <h5><i class="fas fa-shapes"></i> Villa</h5>
+                    <br>
+                    <h5><i class="fas fa-map-marker-alt"></i> Bekasi</h5>
+                    <br>
+                    <h5><i class="fas fa-map-marker-alt" style="visibility: hidden;"></i> Jl Pattimura No 5</h5>
+                    <br>
+                    <h5><i class="fas fa-phone"></i> +62 81234567890</h5>
+                    <br>
+                    <h5><i class="fas fa-link"></i> <a href="#">www.hotelku.co.id</a></h5>
+                    <br>
+                    <h5><i class="fas fa-align-justify"></i> 
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quia cum neque exercitationem sequi eius deleniti consectetur, veniam, dolorem quo cumque necessitatibus saepe, voluptatem perspiciatis debitis porro expedita totam deserunt!
+                    </h5>
+                    <br>
+                    <button type="button" class="btn btn-primary px-3 py-2 rounded-pill">Booking sekarang</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
