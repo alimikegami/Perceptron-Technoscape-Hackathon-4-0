fff7ceb4f540cd22b69de00e4a62feb1a8a9f95a
Alim Ikegami <alimikegami1@gmail.com>
2021-07-03 14:00:40 +0800

    updating database file

--- database_staycay.sql
+++ database_staycay.sql
@@ -21,19 +21,22 @@ CREATE TABLE accommodation (
     accommodation_street_address VARCHAR(50) NOT NULL,
     accommodation_category_id INT NOT NULL,
     accommodation_price_per_night INT NOT NULL,
-    accommodation_max_guest_amount INT NOT NULL,
-    accomodaton_bed_room_amount INT NOT NULL,
-    accomodaton_bath_room_amount INT NOT NULL,
+    accommodation_max_guest_per_housing INT NOT NULL,
+    accommodation_bed_room_amount INT NOT NULL,
+    accommodation_bath_room_amount INT NOT NULL,
+    accommodation_housing_amount INT NOT NULL,
     accommodation_desc TEXT NOT NULL,
     PRIMARY KEY(accommodation_id),
     FOREIGN KEY(accommodation_category_id) REFERENCES accommodation_category(category_id)
 );
 
-CREATE TABLE accommodation_photos (
-    
-    accommodation_image_path VARCHAR(255) NOT NULL,
-    
-)
+CREATE TABLE accommodation_photo (
+    photo_id INT AUTO_INCREMENT,
+    accommodation_id INT,
+    accommodation_image_name VARCHAR(20) NOT NULL,
+    PRIMARY KEY(photo_id),
+    FOREIGN KEY(accommodation_id) REFERENCES accommodation(accommodation_id)
+);
 
 CREATE TABLE rating (
     rating_id INT AUTO_INCREMENT.
@@ -46,7 +49,25 @@ CREATE TABLE rating (
     FOREIGN KEY(user_id) REFERENCES user_account(user_id)
 );
 
+CREATE TABLE event (
+    event_id INT AUTO_INCREMENT,
+    accommodation_id INT NOT NULL,
+    user_id INT NOT NULL,
+    event_title CHAR(75) NOT NULL,
+    event_desc TEXT NOT NULL,
+    PRIMARY KEY(event_id),
+    FOREIGN KEY(accommodation_id) REFERENCES accommodation(accommodation_id),
+    FOREIGN KEY(user_id) REFERENCES user_account(user_id)
+);
+
 CREATE TABLE booking (
+    booking_id INT AUTO_INCREMENT,
+    accommodation_id INT NOT NULL,
+    user_id INT NOT NULL,
+    check_in DATE NOT NULL,
+    check_out DATE NOT NULL,
+    PRIMARY KEY(booking_id),
+    FOREIGN KEY(accommodation_id) REFERENCES accommodation(accommodation_id)
+);
 
 
-)
