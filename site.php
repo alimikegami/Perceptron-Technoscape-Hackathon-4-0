ad03fac5f502a8a3457251ef6b47a06e8fc5a053
Alim Ikegami <alimikegami1@gmail.com>
2021-07-05 03:24:45 +0800

    site.php fix

--- site.php
+++ site.php
@@ -62,50 +62,6 @@ require_once "functions.php";
                 <ion-icon name="star"></ion-icon>
                 <ion-icon name="star"></ion-icon>
 
-                <!-- <button type="button" class="btn login-button" data-toggle="modal" data-target="#modalUlasan">Launch demo modal</button> -->
-                <!-- <div class="modal fade" id="modalUlasan" tabindex="-1" aria-labelledby="labelModalUlasan" aria-hidden="false"> -->
-                <!-- <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Ulas: Glamping Malang Asri</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-
-                            <div class="modal-body">
-                                <div class="row">
-                                    <div class="col">
-                                        <img src="img/hotel1.jpg" class="img-fluid" alt="...">
-                                    </div>
-                                </div>
-                                <br>
-                                <form action="">
-                                <div class="row">
-                                    <div class="col text-center">
-                                        <ion-icon name="star"></ion-icon> 
-                                        <ion-icon name="star"></ion-icon> 
-                                        <ion-icon name="star"></ion-icon> 
-                                        <ion-icon name="star"></ion-icon> 
-                                        <ion-icon name="star"></ion-icon> 
-
-
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="mb-3">
-                                        <label for="ulasan_deskripsi" class="form-label">Ulasan Kamu</label>
-                                        <input type="text" class="form-control align-text-top" style="height: 250px;" id="ulasan_deskripsi" name="review">
-                                    </div>
-                                </div>
-                                </form>
-                               
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-primary" id="">Tambah Ulasan</button>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
-
             </div>
             <div class="col book-input-form">
                 <h2 class="mt-3">Rp200.000,00/Malam</h2>
@@ -159,7 +115,7 @@ require_once "functions.php";
                                 <span id="right-total"></span>
                             </div>
                         </div>
-                        <a class="btn login-button mt-5" id="book" type="button" href="payment_page.php" hidden>Book Now</button>
+                        <a class="btn login-button mt-5" id="book" type="button" href="payment_page.php" hidden>Book Now</a>
                     </div>
                 </form>
             </div>
@@ -173,6 +129,7 @@ require_once "functions.php";
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel mattis massa. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed lorem arcu, bibendum ut euismod quis, malesuada eget nisi. Sed et ante nisl. Nunc id justo eu est malesuada egestas sed vel ex. Aenean eget arcu mauris. Donec ornare magna tortor, in rhoncus mauris elementum id.</p>
             </div>
         </div>
+        
         <div class="row">
             <div class="col-md-8">
                 <hr>
